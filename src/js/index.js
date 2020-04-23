8159c827e04746c395e9e5c5fef2d4e9155657b2
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-23 15:02:18 -0400

    Build search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,25 @@ import Search from "./models/Search";
 
 const state = {};
 
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+const controlSearch = async () => {
+  // 1. Get the query from the view
+  const query = "pizza"; // DOOOO
+
+  if (query) {
+    // 2. New search object and add it to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+
+    // 4. Search for recepies
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    console.log(state.search.results);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent the page from reloading
+  controlSearch();
+});

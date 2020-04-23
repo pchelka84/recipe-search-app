83e81448f580e5f1c1903f3ac9fe4f9d698c159e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-23 14:05:22 -0400

    Add a state to the Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import Search from "./models/Search";
 
+/** GLOBAL STATE OF THE APP
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
 const search = new Search("pizza");
 console.log(search);
-
 search.getResults();

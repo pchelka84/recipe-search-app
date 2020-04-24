0f8696f0adc8db7052dbe6d758149dba584685c1
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-24 11:19:16 -0400

    Clear loader once results are displayed

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,9 +5,13 @@ export const elements = {
   searchResultList: document.querySelector(".results__list"),
 };
 
+export const elementStrings = {
+  loader: "loader",
+};
+
 export const renderLoader = (parent) => {
   const loader = `
-    <div class="loader">
+    <div class="${elementStrings.loader}">
       <svg>
         <use href='img/icons.svg#icon-cw'></use>
       </svg>
@@ -15,4 +19,9 @@ export const renderLoader = (parent) => {
   `;
 
   parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
 };

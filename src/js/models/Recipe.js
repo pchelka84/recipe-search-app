df6544bcc676b63d42540a8e6a7f1981098fdf47
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-25 15:33:17 -0400

    Uniform units

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -32,4 +32,38 @@ export default class Recipe {
   calcServings() {
     this.servings = 4;
   }
+
+  parseIngredients() {
+    const unitsLong = [
+      "tablespoons",
+      "tablespoon",
+      "ounce",
+      "ounces",
+      "teaspoons",
+      "teaspoon",
+      "cups",
+      "ponds",
+    ];
+    const unitsShort = [
+      "tbsp",
+      "tbsp",
+      "oz",
+      "oz",
+      "tsp",
+      "tsp",
+      "cup",
+      "pound",
+    ];
+    const newIngredients = this.ingredients.map((el) => {
+      // 1. Uniform units
+      let ingredient = el.toLowerCase();
+      unitsLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, unitShort[i]);
+      });
+
+      // 2. Remove parentheses
+      // 3. Parse ingredients into count, unit and ingredients
+    });
+    this.ingredients = newIngredients;
+  }
 }

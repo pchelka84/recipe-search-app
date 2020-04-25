36324e524da9b8ba6ff15a7cec2b4a2c885a13bb
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-25 14:15:43 -0400

    Get clicked recipe data

--- src/js/index.js
+++ src/js/index.js
@@ -59,6 +59,29 @@ elements.searchResPages.addEventListener("click", (e) => {
  * RECIPE CONTROLLER
  **/
 
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+// const r = new Recipe(46956);
+// r.getRecipe();
+// console.log(r);
+const controlRecipe = async () => {
+  // Get ID from URL
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    // Get recipe data
+    await state.recipe.getRecipe();
+
+    // Calculate servings and time
+    state.recipe.calcTime();
+    state.recipe.calcServings();
+
+    // Render recipe
+    console.log(state.recipe);
+  }
+};
+
+window.addEventListener("hashchange", controlRecipe);

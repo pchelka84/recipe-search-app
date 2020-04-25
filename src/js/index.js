43db888f2ea39c1527752851f83c590157467304
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-25 13:12:48 -0400

    Build a recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -11,6 +12,9 @@ import { elements, renderLoader, clearLoader } from "./views/base";
 
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ **/
 const controlSearch = async () => {
   // 1. Get the query from the view
   const query = searchView.getInput();
@@ -50,3 +54,7 @@ elements.searchResPages.addEventListener("click", (e) => {
     // console.log(goToPage);
   }
 });
+
+/**
+ * RECIPE CONTROLLER
+ **/

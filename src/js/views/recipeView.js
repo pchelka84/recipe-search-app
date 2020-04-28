6f37cb675cc6c9e2cae883a117e9c5a64e63525f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-28 10:07:19 -0400

    Update recipe servings

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -71,12 +71,12 @@ export const renderRecipe = (recipe) => {
                     <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
-                        <button class="btn-tiny">
+                        <button class="btn-tiny  btn-decrease">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-minus"></use>
                             </svg>
                         </button>
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-increase">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-plus"></use>
                             </svg>
@@ -132,4 +132,16 @@ export const renderRecipe = (recipe) => {
   `;
 
   elements.recipe.insertAdjacentHTML("afterbegin", markup);
+};
+
+export const updateServingsIngredients = (recipe) => {
+  // Update servings
+  document.querySelector(".recipe__info-data--people").textContent =
+    recipe.servings;
+
+  // Update ingredients
+  const countElement = Array.from(document.querySelectorAll("recipe__count"));
+  countElement.forEach((el, i) => {
+    el.textContent = formatCount(recipe.ingredients[i].count);
+  });
 };

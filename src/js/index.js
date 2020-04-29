7cab7317d152a1474eca60eb9ef9f2c1a6bba659
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-29 09:58:16 -0400

    Add each ingredient to the list & UI

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** GLOBAL STATE OF THE APP
@@ -124,6 +125,20 @@ const controlRecipe = async () => {
   window.addEventListener(event, controlRecipe);
 });
 
+/**
+ * LIST CONTROLLER
+ * */
+const controlList = () => {
+  // Create a new list IF there is none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach((el) => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", (e) => {
   if (e.target.matches(".btn-decrease, .btn-decrease *")) {
@@ -136,8 +151,10 @@ elements.recipe.addEventListener("click", (e) => {
     // Increase button is clicked
     state.recipe.updateServings("inc");
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+    // Add ingredients to shopping list
+    controlList();
   }
-  // console.log(state.recipe);
 });
 
 window.l = new List();
--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -20,5 +20,5 @@ export const renderItem = (item) => {
 
 export const deleteItem = (id) => {
   const item = document.querySelector(`[data-itemid="${id}"]`);
-  ite,.parentElement.removeChild(item);
+  if (item) item.parentElement.removeChild(item);
 };

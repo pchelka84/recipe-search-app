f26acf6c79976ed6ac3310147d141910872a0adc
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-04-30 13:35:20 -0400

    Restore likes from localStorage

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -32,4 +32,11 @@ export default class Likes {
   persistData() {
     localStorage.setItem("likes", JSON.stringify(this.likes));
   }
+
+  readStorage() {
+    const storage = JSON.parse(localStorage.getItem("likes"));
+
+    // Restore data from the local storage
+    if (storage) this.likes = storage;
+  }
 }
